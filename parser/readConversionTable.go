eb8aa423968d6c98cfc42a0d4468a8d7aae6ca37
sofyalaski <sofyalaskina@gmail.com>
2025-08-18 16:55:49 +0200

    change for the updated conversions table

--- parser/readConversionTable.go
+++ parser/readConversionTable.go
@@ -7,19 +7,6 @@ import (
 	"strings"
 )
 
-func stringJoiner(stringsArray []string) string {
-	var joinedString string
-	for _, s := range stringsArray {
-		if s != "" {
-			if joinedString != "" {
-				joinedString += "."
-			}
-			joinedString += strings.TrimSpace(s) // trim all leading and trailing whitespaces
-		}
-	}
-	return joinedString
-}
-
 // ConversionTableReadColumn return a slice of column values in a table.
 // It takes a path to a CSV file and column name as an argument.
 func ConversionTableReadColumn(path string, column string) ([]string, error) {
@@ -37,37 +24,17 @@ func ConversionTableReadColumn(path string, column string) ([]string, error) {
 		return columnValues, err
 	}
 
-	// in the table header extract the index of the column
-	var fieldsStart int
-	var fieldsEnd int
-	var colExists bool
-
 	for j := range records[0] {
 		// Since conversions table is often handled from Microsoft Excel and saved in UTF-8, it adds a byte-order mark (BOM) at the beginning of file, which is invisible.
 		// for first column in header this needs to be removed from the name of column!
 		if j == 0 {
 			records[0][j] = strings.Trim(records[0][j], string('\uFEFF'))
 		}
-		if records[0][j] == column {
-			fieldsStart = j
-			colExists = true
-			break
-		}
-	}
-	for j := range records[0][fieldsStart+1:] {
-		if records[0][fieldsStart+1+j] != "" {
-			fieldsEnd = fieldsStart + 1 + j
-			break
-		}
-	}
-
-	if !colExists {
-
-		return columnValues, fmt.Errorf("column %s does not exist in table %s", column, path)
 	}
 
 	for _, r := range records[1:] {
-		columnValues = append(columnValues, stringJoiner(r[fieldsStart:fieldsEnd]))
+		columnValues = append(columnValues, r[0])
 	}
+	fmt.Println("Column values:", columnValues)
 	return columnValues, nil
 }
